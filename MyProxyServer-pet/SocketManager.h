2bcdf7e2d19af00e442a50061864304f2372aed4
Илья Переход <ilya.perekhod@gmail.com>
2023-12-02 21:43:14 +0500

    added error handling to the handleData method

--- MyProxyServer-pet/SocketManager.h
+++ MyProxyServer-pet/SocketManager.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <boost/asio.hpp>
+#include <iostream>
 
 class SocketManager 
 {
@@ -14,10 +15,16 @@ public:
     // Обработчик завершения операции async_accept
     void handleAccept(const boost::system::error_code& error);
 
+    // Обработчик данных
+    void handleData(const boost::system::error_code& error, std::size_t bytes_transferred);
+
 private:
     // Acceptor для прослушивания входящих соединений
     boost::asio::ip::tcp::acceptor acceptor_;   
 
     // Сокет для обработки соединений
     boost::asio::ip::tcp::socket socket_;
+
+    // Буфер для приема/отправки данных
+    std::array<char, 8192> buffer_;
 };
