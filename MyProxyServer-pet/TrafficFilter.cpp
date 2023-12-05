7599792aa0012c84014a1c35bb59f5990a28a109
Илья Переход <ilya.perekhod@gmail.com>
2023-12-05 19:05:22 +0500

    add TrafficFilter.h and .cpp

---
+++ MyProxyServer-pet/TrafficFilter.cpp
@@ -0,0 +1,19 @@
+﻿#include "TrafficFilter.h"
+#include <unordered_set>
+
+namespace 
+{
+    // Здесь мы определяем список заблокированных URL-адресов
+    const std::unordered_set<std::string> blockedUrls = 
+    {
+        "google.com",
+        "yandex.ru"
+        // Добавьте сюда другие URL-адреса, которые вы хотите заблокировать
+    };
+}
+
+bool TrafficFilter::isUrlBlocked(const std::string& url) const 
+{
+    // Проверяем, находится ли URL в списке заблокированных
+    return blockedUrls.find(url) != blockedUrls.end();
+}
---
+++ MyProxyServer-pet/TrafficFilter.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <string>
+
+class TrafficFilter 
+{
+public:
+    TrafficFilter() = default;
+    ~TrafficFilter() = default;
+
+    bool isUrlBlocked(const std::string& url) const;
+};
